1e4d5f7491de9fcf3931a34cade79addefe8380d
Charlie Wang <charlie.wang@edu.uwaterloo.ca>
2019-03-29 22:26:07 -0400

    Fix annoyances and bugs
    - Archives all visible channels instead of a single channel at a time
    - Fix message count logic for first commit
    - Save attachments as json blob
    - Fix timestamps being double-coverted to local time
    - Add code for saving all future messages, instead of all past messages
      (TODO: expose this)

--- dscrape.py
+++ dscrape.py
@@ -1,10 +1,13 @@
 import argparse
 import asyncio
 import configparser
+import json
 import sqlite3
+from datetime import timezone
 
 import discord
 
+# See https://discordpy.readthedocs.io/en/v0.16.12/api.html#user
 USERS_TABLE_DDL = \
     """
 CREATE TABLE IF NOT EXISTS users(
@@ -14,6 +17,7 @@ CREATE TABLE IF NOT EXISTS users(
     discriminator TEXT NOT NULL
 );"""
 
+# See https://discordpy.readthedocs.io/en/v0.16.12/api.html#channel
 CHANNELS_TABLE_DDL = \
     """
 CREATE TABLE IF NOT EXISTS channels(
@@ -21,6 +25,8 @@ CREATE TABLE IF NOT EXISTS channels(
     channel_name NOT NULL
 );"""
 
+# See https://discordpy.readthedocs.io/en/v0.16.12/api.html#message
+# TODO: attachments table?
 MESSAGES_TABLE_DDL = \
     """
 CREATE TABLE IF NOT EXISTS messages(
@@ -31,17 +37,18 @@ CREATE TABLE IF NOT EXISTS messages(
     channel_id INTEGER NOT NULL,
     content TEXT,
     clean_content TEXT,
+    attachments BLOB,
     FOREIGN KEY(author_id) REFERENCES users(id),
     FOREIGN KEY(channel_id) REFERENCES channels(id)
 );"""
 
 
 class MyClient(discord.Client):
-    def __init__(self, channel, *args, **kwargs):
+    def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self._started = False
-        self._channel = channel
-        self._conn = sqlite3.connect("messages.sqlite3")
+
+        self._conn = sqlite3.connect("discord_archive.sqlite3")
         self._conn.execute("PRAGMA foreign_keys = ON;")
         self._conn.execute(USERS_TABLE_DDL)
         self._conn.execute(CHANNELS_TABLE_DDL)
@@ -53,55 +60,81 @@ class MyClient(discord.Client):
 
     async def commit_task(self):
         await self.wait_until_ready()
-        last_count = 0
+        last_count = int(
+            next(self._conn.execute("SELECT COUNT(*) FROM messages;"))[0])
+
         while not self.is_closed:
             self._conn.commit()
             for row in self._conn.execute("SELECT COUNT(*) FROM messages;"):
                 count = int(row[0])
                 diff = count - last_count
-                if diff:
-                    print("Got {} messages with total {}".format(
-                          diff, count))
-                else:
-                    print("Got nothing with total {}".format(count))
+                print("Got {} messages with total {}".format(diff, count))
                 last_count = count
             await asyncio.sleep(5)
 
     async def user_tuple_generator(self):
         """Yields tuples to be inserted into the users table."""
         for user in self.get_all_members():
-            yield (
-                int(user.id),
-                user.name,
-                user.display_name,
-                user.discriminator
-            )
+            yield (int(user.id), user.name, user.display_name,
+                   user.discriminator)
 
     async def channel_tuple_generator(self):
         """Yields tuples to be inserted into the channels table."""
         for channel in self.get_all_channels():
             yield (int(channel.id), channel.name)
 
-    async def message_tuple_generator(self, before=None):
+    async def archive_permission(self, channel):
+        """Return if we have read and history permission for a Channel."""
+        perms = channel.permissions_for(channel.server.me)
+        return perms.read_messages and perms.read_message_history
+
+    async def message_tuple_generator(self, update=False):
         """Yields tuples to be inserted into the messages/users table.
 
+        If update is true, grab messages after the latest message in the table
+        for the channel. Otherwise, grab messages before the oldest message in
+        the table for the channel.
+
         Yields a tuple of (message tuple, user tuple)."""
-        channel = discord.Object(self._channel)
-        async for message in self.logs_from(channel, before=before,
-                                            limit=99999999):
-            yield (
-                (int(message.id),
-                 message.timestamp.timestamp(),
-                 int(message.author.id),
-                 message.author.name,
-                 int(message.channel.id),
-                 message.content,
-                 message.clean_content),
-                (message.author.id,
-                 message.author.name,
-                 message.author.display_name,
-                 message.author.discriminator)
-            )
+        # Archive all channels that we have access to
+        channels = (c for c in self.get_all_channels()
+                    if await self.archive_permission(c))
+
+        async for channel in channels:
+            print("Archiving channel {}".format(channel.name))
+
+            before, after = None, None
+            if update:
+                for row in self._conn.execute(
+                        """
+                  SELECT id, max(timestamp) FROM messages
+                  WHERE channel_id = ?""", (int(channel.id), )):
+                    after = discord.Object(row[0])
+
+            else:
+                for row in self._conn.execute(
+                        """
+                  SELECT id, min(timestamp) FROM messages
+                  WHERE channel_id = ?""", (int(channel.id), )):
+                    before = discord.Object(row[0])
+
+            async for message in self.logs_from(
+                    channel, before=before, after=after, limit=999999999):
+
+                # We want a SQL NULL instead of a string "[]"
+                if message.attachments:
+                    attachments = json.dumps(message.attachments)
+                else:
+                    attachments = None
+                # Get UTC unix timestamp from a naive datetime object
+                ts = message.timestamp.replace(tzinfo=timezone.utc).timestamp()
+                yield ((int(message.id), ts,
+                        int(message.author.id), message.author.name,
+                        int(message.channel.id), message.content,
+                        message.clean_content, attachments),
+                       (message.author.id, message.author.name,
+                        message.author.display_name,
+                        message.author.discriminator))
 
     async def on_ready(self):
         if self._started:
@@ -113,48 +146,32 @@ class MyClient(discord.Client):
             print("Inserting channels")
             async for t in self.channel_tuple_generator():
                 self._conn.execute(
-                    "INSERT OR IGNORE INTO channels VALUES(?, ?)", t
-                )
+                    "INSERT OR IGNORE INTO channels VALUES(?, ?)", t)
 
             print("Inserting users")
             users = set()  # Keep track of user ids we've inserted
             async for t in self.user_tuple_generator():
                 users.add(t[0])
                 self._conn.execute(
-                    "INSERT OR IGNORE INTO users VALUES(?, ?, ?, ?)", t
-                )
+                    "INSERT OR IGNORE INTO users VALUES(?, ?, ?, ?)", t)
 
             self._conn.commit()
 
             print("Inserting messages")
-            for row in self._conn.execute(
-                "SELECT id, min(timestamp) FROM messages "
-                "GROUP BY channel_id "
-                "HAVING channel_id = ?",
-                (self._channel,)
-            ):
-                earliest_message = discord.Object(row[0])
-                print("Resuming")
-                break
-            else:
-                earliest_message = None
 
-            async for t in \
-                    self.message_tuple_generator(before=earliest_message):
+            async for t in self.message_tuple_generator():
                 # Users that have left the server won't be in the users table,
                 # and this breaks the foreign key constraint. We insert them
                 # here if necessary.
                 message, user = t
                 if user[0] not in users:
                     self._conn.execute(
-                        "INSERT OR IGNORE INTO users VALUES (?, ?, ?, ?)", user
-                    )
+                        "INSERT OR IGNORE INTO users VALUES (?, ?, ?, ?)",
+                        user)
                     users.add(user[0])
                 self._conn.execute(
                     "INSERT OR IGNORE INTO messages VALUES "
-                    "(?, ?, ?, ?, ?, ?, ?)",
-                    message
-                )
+                    "(?, ?, ?, ?, ?, ?, ?, ?)", message)
             self._conn.commit()
 
             print("Done!")
@@ -177,7 +194,7 @@ def try_config(config, heading, key):
         value = section[key]
     except KeyError:
         exit("Missing config key '{}' under section '[{}]'".format(
-             key, heading))
+            key, heading))
 
     return value
 
@@ -191,9 +208,8 @@ def main():
     config = configparser.ConfigParser()
     config.read(args.config)
     token = try_config(config, "MAIN", "Token")
-    channel = try_config(config, "MAIN", "Channel")
 
-    client = MyClient(channel)
+    client = MyClient()
     client.run(token)
 
 
