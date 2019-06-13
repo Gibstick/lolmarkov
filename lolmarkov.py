0cff67ac8b59d1bd4a453b114151716ec6ded960
Charlie Wang <charlie.wang@edu.uwaterloo.ca>
2019-06-13 18:12:46 -0400

    Switch to aiosqlite and async_lru

--- lolmarkov.py
+++ lolmarkov.py
@@ -1,24 +1,25 @@
 import argparse
 import configparser
 import functools
-import sqlite3 #TODO: aiosqlite3
+import aiosqlite
 import sys
+import traceback
 from collections import namedtuple
 
 import discord
 import markovify
+from async_lru import alru_cache
 from discord.ext import commands
 
 import util
 
 
-@functools.lru_cache(maxsize=32)
+@alru_cache(maxsize=32)
 async def create_model(author_id: int, conn):
-    # TODO: async
-    c = conn.cursor()
-
-    messages = c.execute("SELECT content FROM messages WHERE author_id is ?",
-                         (author_id, )).fetchall()
+    cursor = await conn.execute(
+        "SELECT content FROM messages WHERE author_id is ?",
+        (author_id, ))
+    messages = await cursor.fetchall()
 
     if len(messages) < 25:
         return None
@@ -31,14 +32,13 @@ DuckUser = namedtuple("DuckUser", ["id", "name", "discriminator"])
 
 async def database_user(conn, argument):
     """"Look up a user in the database and return a NamedTuple mimicking a discord.User"""
-    c = conn.cursor()
-    users = c.execute(
+    cursor = await c.execute(
         "SELECT id, username, discriminator FROM users WHERE username||'#'||discriminator == ? LIMIT 1",
-        (argument, )).fetchall()
+        (argument, ))
+    users = await cursor.fetchone()
 
     if not users:
-        raise commands.CommandError(
-            f"User {argument} not found in data set.")
+        raise commands.CommandError(f"User {argument} not found in data set.")
 
     user = users[0]
     member = DuckUser(id=user[0], name=user[1], discriminator=user[2])
@@ -48,20 +48,22 @@ async def database_user(conn, argument):
 class MarkovCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self._conn = sqlite3.connect("file:./discord_archive.sqlite3?mode=ro",
-                                     uri=True)
+        self._conn = None  # initialized in on_ready
         self._model = None
         self._model_attrib = None
         self._user_converter = commands.UserConverter()
 
     @commands.Cog.listener()
     async def on_ready(self):
+        if self._conn is None:
+            self._conn = await aiosqlite.connect(
+                "file:./discord_archive.sqlite3?mode=ro", uri=True)
+
         if self._model_attrib is None:
             print("Resetting nickname(s)...")
             for guild in self.bot.guilds:
                 await guild.me.edit(nick=None)
         print("Ready!")
-
 
     async def set_name(self, ctx, member):
         """Change nickname to indicate that current model is for member."""
@@ -70,7 +72,6 @@ class MarkovCog(commands.Cog):
         model_attrib = f"{member.name}#{member.discriminator}"
         self._model_attrib = model_attrib
         await me.edit(nick=f"{basename} ({model_attrib})")
-
 
     @commands.command()
     async def switch(self, ctx, *, arg: str):
@@ -101,6 +102,7 @@ class MarkovCog(commands.Cog):
         else:
             await ctx.send("Unable to switch data set.")
             print(error)
+            traceback.print_exc()
         await ctx.message.add_reaction("❌")
 
     @commands.command()
@@ -123,7 +125,7 @@ class MarkovCog(commands.Cog):
                 sentence = None
 
         if sentence:
-            await ctx.send(f"{sentence}\n- {self._model_attrib}")
+            await ctx.send(f"{sentence}\n`{self._model_attrib}`")
         else:
             await ctx.message.add_reaction("❌")
             await ctx.send("Unable to get sentence.", delete_after=3)
